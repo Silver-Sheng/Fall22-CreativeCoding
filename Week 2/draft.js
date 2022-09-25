e127ea72975d90510741b8f5659a749e5c58953e
ruoyun sheng <rs8442@nyu.edu>
2022-09-24 23:21:49 -0400

    init commit
    added template code

---
+++ Week 2/draft.js
@@ -0,0 +1,56 @@
+// function draw() {
+//     background(220)
+
+//     push()
+//     translate(300, 300)
+//     rotate(millis()/1000)
+//     rectMode(CENTER)
+//     rect(0, 0, 300, 300)
+//     ellipseMode(CENTER)
+//     ellipse(0, 0, 300, 300)
+// }
+
+
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight);
+// }
+
+
+
+// function draw() {
+//     let hrPointer = hour();
+//     let minPointer = minute();
+//     let secPointer = second();
+
+//     translate(300, 300);
+//     rotate(-90)
+
+//     noStroke();
+//     fill(255);
+//     ellipse(0, 0, 400, 400);
+
+//     stroke(100);
+//     noFill();
+//     let secAngle = map(secPointer, 0, 60, 0, 360);
+//     arc(0, 0, 390,390, 0, secAngle);
+
+//     stroke(50);
+//     noFill();
+//     let minAngle = map(minPointer, 0, 60, 0, 360);
+//     arc(0, 0, 390,390, 0, minAngle);
+
+//     stroke(0);
+//     noFill();
+//     let hrAngle = map(hrPointer, 0, 12, 0, 360);
+//     arc(0, 0, 390,390, 0, hrAngle);
+
+//     push()
+//     rotate
+//     stroke(secAngle);
+//     line(0, 0, 100, 0);
+//     pop();
+
+
+
+// }
---
+++ Week 2/script.js
@@ -0,0 +1,78 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    angleMode(DEGREES)
+}
+
+function draw() {
+    background(244, 236, 213);
+    translate(300, 300);
+    rotate(-90);
+
+    let hrPointer = hour();
+    let minPointer = minute();
+    let secPointer = second();
+
+    noStroke();
+    fill(255, 150)
+    ellipse(0, 0, 370, 370);
+
+    strokeWeight(6);
+    noFill();
+    stroke(166, 178, 165);
+    let end1 = map(secPointer, 0, 60, 0, 360)
+    arc(0, 0, 370, 370, 0, end1);
+
+    strokeWeight(8);
+    stroke(249, 218, 136);
+    let end2 = map(minPointer, 0, 60, 0, 360)
+    arc(0, 0, 350, 350, 0, end2);
+
+    strokeWeight(10);
+    stroke(172, 26, 50);
+    let end3 = map(hrPointer % 12, 0, 24, 0, 360)
+    arc(0, 0, 325, 325, 0, end3);
+
+    push();
+    rotate(end1);
+    stroke(166, 178, 165);
+    strokeWeight(6);
+    line(0, 0, 125, 0);
+    pop();
+
+    push();
+    rotate(end2);
+    stroke(249, 218, 136);
+    strokeWeight(8);
+    line(0, 0, 100, 0);
+    pop();
+
+    push();
+    rotate(end3);
+    stroke(172, 26, 50);
+    strokeWeight(10);
+    line(0, 0, 70, 0);
+    pop();
+
+    push();
+    rotate(90);
+    noStroke();
+    fill(172, 26, 50);
+    textSize(17);
+    text('12', -10, -135);
+
+    textSize(17);
+    text('3', 135, 5);
+    
+    textSize(17);
+    text('6', -5, 145);
+
+    textSize(17);
+    text('9', -145, 5);
+
+
+    
+}
+
+
+
+
