bb206dd60bc5d8288f059e776712a270bf9cfe5e
ruoyun sheng <rs8442@nyu.edu>
2022-10-16 18:48:44 -0400

    week5

--- Week 5/script.js
+++ Week 5/script.js
@@ -1,43 +1,68 @@
-let toggle = false
+// let toggle = false
 
-let dog;
-
-
-
+let dogImg;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    fetch("https://dog.ceo/api/breeds/image/random").then((response) => {
-        console.log("response recived")
-        console.log(response)
-        return response.json()
-    }).then((data) => {
-        console.log("json parsed!")
-        console.log(data)
-    })
+    // fetch("https://dog.ceo/api/breeds/image/random").then((response) => {
+    //     console.log("response recived")
+    //     console.log(response)
+    //     return response.json()
+    // }).then((data) => {
+    //     console.log("json parsed!")
+    //     console.log(data)
 
-    setTimeout(doThisLater, 4000)
+    //     dogImg = loadImage(data.message)
+
+    // })
+
+    getDog("https://dog.ceo/api/breeds/image/random")
+
+    // setTimeout(doThisLater, 4000)
     
-    setTimeout(doThisEvenLater, 6000)
+    // setTimeout(doThisEvenLater, 6000)
 
-    setTimeout(() => {
-        console.log("Hello, again")
-        toggle = true
-    }, 8000)
+    // setTimeout(() => {
+    //     console.log("Hello, again")
+    //     toggle = true
+    // }, 8000)
 }
 
 function draw() {
     background(0);
-    if(toggle) ellipse(100, 100, 100)
+
+    if (dogImg != undefined) {
+        let dogWidth = dogImg.width
+        let dogHeight = dogImg.height
+
+        if (dogImg.height > height) {
+            dogWidth /= 2
+            dogHeight /= 2
+        }
+        image(dogImg, 0, 0, dogWidth, dogHeight)
+    }
+    
 }
 
-function doThisLater() {
-    console.log("hello, world!!!!");
-    toggle = true
+async function getDog(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    dogImg = loadImage(data.message)
 }
 
 
-const doThisEvenLater = () => {}
 
-const sum = (a, b) => a + b;
+
+
+
+
+// function doThisLater() {
+//     console.log("hello, world!!!!");
+//     toggle = true
+// }
+
+
+// const doThisEvenLater = () => {}
+
+// const sum = (a, b) => a + b;
